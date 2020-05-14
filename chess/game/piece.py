ea1e335c01a68cd5d7866f143f237a0ebc2ab0c4
ryomakawakami <ryomakwkm@gmail.com>
2020-05-13 21:03:38 -0400

    Constructor for pieces and a function

--- chess/game/piece.py
+++ chess/game/piece.py
@@ -1,4 +1,9 @@
 class Piece:
-    def __init__(self):
+    def __init__(self, pieceType, color, position):
+        self.pieceType = pieceType
+        self.color = color
+        self.position = position
+        self.moves = []
+
+    def updateMoves(self, pieces):
         pass
-    
